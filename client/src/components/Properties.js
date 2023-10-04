0916f0108078eb19478357ad233c5239587f3daa
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-10-04 23:22:59 +0300

    Created initial base styling for pages/sections

--- client/src/components/Properties.js
+++ client/src/components/Properties.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import './Base.css'
 
 function Properties() {
   return (
-    <div>Properties</div>
+    <div className='container'>
+      <p className='section-title'>properties</p>
+      <p className='header'>RENTAL|TRACKER</p>
+    </div>
   )
 }
 
