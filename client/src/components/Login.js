d0c3bdfcec9f59a206ff5cd8e287ac17fae6a691
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-10-06 03:51:14 +0300

    Implemented react auth kit sign in

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,16 +2,47 @@ import React, { useState } from 'react';
 import '../App.css';
 import email_icon from '../Images/email_icon.png';
 import password_icon from '../Images/password_icon.png';
+import { useSignIn } from 'react-auth-kit'
 
 function Login() {
+  const signIn = useSignIn()
   let [email, setEmail] = useState('')
   let [password, setPassword] = useState('')
+
+  userInfo = {
+    email: email,
+    password: password
+  }
 
   function handleSubmit(e) {
     e.preventDefault()
     e.stopPropagation()
 
-    // Handle requests
+    fetch('http://127.0.0.1:5000/login', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userInfo)
+    })
+      .then(res => {
+        //handle status code check
+        return res.json()
+      })
+      .then(data => {
+        if (data.access_token) {
+          console.log(data.access_token)
+          signIn({
+            token: data.access_token,
+            expiresIn: 3600,
+            tokenType: "Bearer",
+            authState: { email: email }
+          })
+        } else {
+          // handle redirect or error message
+        }
+      })
+
   }
 
   return (
@@ -33,7 +64,7 @@ function Login() {
           <div className='forgot-password'>Lost Password?<span> Click Here!</span></div>
 
           <div className='submit-container'>
-            <input type='submit' className='submit' value='Log in'/>
+            <input type='submit' className='submit' value='Log in' />
           </div>
         </div>
       </div>
