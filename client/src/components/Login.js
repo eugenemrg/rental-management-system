16de729427862cb58660a5528b5f585804cdf5d5
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-10-06 03:42:46 +0300

    Added functionality to onboarding forms

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,31 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 import email_icon from '../Images/email_icon.png';
 import password_icon from '../Images/password_icon.png';
 
 function Login() {
+  let [email, setEmail] = useState('')
+  let [password, setPassword] = useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    e.stopPropagation()
+
+    // Handle requests
+  }
+
   return (
-    <div className='login'>
-      <div className='header'>
-        <div className="text">Login</div>
-        <div className="underline"></div> 
+    <form onSubmit={handleSubmit}>
+      <div className='login'>
+        <div className='header'>
+          <div className="text">Login</div>
+          <div className="underline"></div>
+        </div>
+        <div className="inputs">
+          <div className="input">
+            <img src={email_icon} alt="" className='icon' />
+            <input type="email" placeholder='Email address' value={email} onChange={e => setEmail(e.target.value)} />
+          </div>
+          <div className="input">
+            <img src={password_icon} alt="" className='icon' />
+            <input type="password" placeholder='Password' value={password} onChange={e => setPassword(e.target.value)} />
+          </div>
+          <div className='forgot-password'>Lost Password?<span> Click Here!</span></div>
+
+          <div className='submit-container'>
+            <input type='submit' className='submit' value='Log in'/>
+          </div>
+        </div>
       </div>
-      <div className="inputs">
-        <div className="input">
-          <img src={email_icon} alt="" className='icon'/>
-          <input type="email" placeholder='Email address'/>
-        </div>
-        <div className="input">
-          <img src={password_icon} alt="" className='icon'/>
-          <input type="password" placeholder='Password'/>
-        </div>
-        <div className='forgot-password'>Lost Password?<span> Click Here!</span></div>
-        
-        <div className='submit-container'> 
-          <div className='submit'>Log in</div>
-        </div>
-      </div>
-    </div>
+    </form>
   );
 }
 
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,39 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 import user_icon from '../Images/user_icon.png';
 import email_icon from '../Images/email_icon.png';
 import password_icon from '../Images/password_icon.png';
 
 function SignUp() {
+  let [name, setName] = useState('')
+  let [email, setEmail] = useState('')
+  let [password, setPassword] = useState('')
+  let [confirmPassword, setConfirmPassword] = useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    e.stopPropagation()
+
+    // Handle requests
+  }
+
   return (
-    <div className='login'>
-      <div className='header'>
-        <div className="text">Sign Up</div>
-        <div className="underline"></div> 
+    <form onSubmit={handleSubmit}>
+      <div className='login'>
+        <div className='header'>
+          <div className="text">Sign Up</div>
+          <div className="underline"></div>
+        </div>
+        <div className="inputs">
+          <div className="input">
+            <img src={user_icon} alt="" className='icon' />
+            <input type="text" placeholder='Name' value={name} onChange={e => setName(e.target.value)} />
+          </div>
+          <div className="input">
+            <img src={email_icon} alt="" className='icon' />
+            <input type="email" placeholder='Email address' value={email} onChange={e => setEmail(e.target.value)} />
+          </div>
+          <div className="input">
+            <img src={password_icon} alt="" className='icon' />
+            <input type="password" placeholder='Password' value={password} onChange={e => setPassword(e.target.value)} />
+          </div>
+          <div className="input">
+            <img src={password_icon} alt="" className='icon' />
+            <input type="password" placeholder='Confirm password' value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} />
+          </div>
+
+          <div className='submit-container'>
+          <input type='submit' className='submit' value='Sign Up'/>
+          </div>
+        </div>
       </div>
-      <div className="inputs">
-        <div className="input">
-          <img src={user_icon} alt="" className='icon'/>
-          <input type="text" placeholder='Name'/>
-        </div>
-        <div className="input">
-          <img src={email_icon} alt="" className='icon'/>
-          <input type="email" placeholder='Email address'/>
-        </div>
-        <div className="input">
-          <img src={password_icon} alt="" className='icon'/>
-          <input type="password" placeholder='Password'/>
-        </div>
-        <div className="input">
-          <img src={password_icon} alt="" className='icon'/>
-          <input type="password" placeholder='Confirm password'/>
-        </div>
-        
-        <div className='submit-container'> 
-          <div className='submit'>Sign Up</div>
-        </div>
-      </div>
-    </div>
+    </form>
   );
 }
 
