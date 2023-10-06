5f9f8fa7d1819150a0cf6086be5ba28fcbc43dd1
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-10-06 04:05:21 +0300

    Implemented private/protected routes on the react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import {Route, Routes} from 'react-router-dom'
+import { RequireAuth } from 'react-auth-kit'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Landing from './components/Landing'
@@ -17,11 +18,31 @@ function App() {
         <Route path='/' element={ <Landing/> } />
         <Route path='login' element={<Login />} />
         <Route path='signup' element={<SignUp />} />
-        <Route path='dashboard' element={ <Home/> } />
-        <Route path='properties' element={ <Properties/> } />
-        <Route path='houses' element={ <Houses/> } />
-        <Route path='issues' element={ <Reports/> } />
-        <Route path='tenants' element={ <Tenants/> } />
+        <Route path='dashboard' element={ 
+          <RequireAuth loginPath='/login'>
+            <Home/>
+          </RequireAuth>
+         } />
+        <Route path='properties' element={ 
+          <RequireAuth loginPath='/login'>
+            <Properties/>
+          </RequireAuth>
+         } />
+        <Route path='houses' element={ 
+          <RequireAuth loginPath='/login'>
+            <Houses/>
+          </RequireAuth>
+         } />
+        <Route path='issues' element={ 
+          <RequireAuth loginPath='/login'>
+            <Reports/>
+          </RequireAuth>
+         } />
+        <Route path='tenants' element={ 
+          <RequireAuth loginPath='/login'>
+            <Tenants/>
+          </RequireAuth>
+         } />
       </Routes>
     </div>
   );
